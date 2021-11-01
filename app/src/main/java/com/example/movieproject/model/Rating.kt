4e289da45fc1725b5d7c51de9a8ef81ed36c96f9
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-11-01 19:11:49 +0300

    implementations added and models created

---
+++ app/src/main/java/com/example/movieproject/model/Rating.kt
@@ -0,0 +1,11 @@
+package com.example.movieproject.model
+
+
+import com.google.gson.annotations.SerializedName
+
+data class Rating(
+    @SerializedName("Source")
+    val source: String,
+    @SerializedName("Value")
+    val value: String
+)
