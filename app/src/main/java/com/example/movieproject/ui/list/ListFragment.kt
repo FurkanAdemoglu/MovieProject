7c3e3ff4955c394d33a5309b0b353eb68462cd29
Furkan Ademoglu <furkanademoglu69@gmail.com>
2021-11-01 19:37:32 +0300

    Fragment list created and ui part coded

---
+++ app/src/main/java/com/example/movieproject/ui/list/ListFragment.kt
@@ -0,0 +1,13 @@
+package com.example.movieproject.ui.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.movieproject.R
+
+
+class ListFragment : Fragment() {
+
+}
